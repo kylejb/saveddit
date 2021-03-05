411128c9dad11dd82dd6d303427e69ea7ae212c4
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-04 20:54:18 -0500

    Render LinkRedditForm to prototype/test OAuth flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import LinkRedditForm from 'LinkRedditForm';
+
 function App() {
-  return <h1>App Component</h1>;
+  return (
+    <>
+      <h1>App Component</h1>
+      <LinkRedditForm />
+    </>
+  );
 }
 
 export default App;
