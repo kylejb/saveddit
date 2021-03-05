2c9ea3a1b379aeeffc8cff318933c5adb7ccd5a7
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-04 21:24:39 -0500

    Scaffolded basic structure for OAuth via server

--- client/src/LinkRedditForm.tsx
+++ client/src/LinkRedditForm.tsx
@@ -1,7 +1,17 @@
 function LinkRedditForm() {
+  /*
+  TODO:
+
+    * func will to trigger OAuth request via backend and return OAuthURL
+    * client will be redirected to Reddit to grant application permission
+    * backend will listen auto redirect on approval to store token
+  **/
+  const redditOAuthHandler = async () => null;
+
   return (
     <>
       <h1>Link Reddit Form</h1>
+      <button onClick={redditOAuthHandler}>Link your Reddit Account</button>
     </>
   );
 }
