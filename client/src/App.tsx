75abe968dd1970a8d3835eda0c2ff578a98ff1e1
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-10 18:05:50 -0500

    FE: Reformatted with eslint and prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import LinkRedditForm from 'LinkRedditForm';
 
 function App() {
-  return (
-    <>
-      <h1>App Component</h1>
-      <LinkRedditForm />
-    </>
-  );
+    return (
+        <>
+            <h1>App Component</h1>
+            <LinkRedditForm />
+        </>
+    );
 }
 
 export default App;
--- client/src/LinkRedditForm.tsx
+++ client/src/LinkRedditForm.tsx
@@ -1,19 +1,20 @@
+/*
+TODO:
+
+  * func will to trigger OAuth request via backend and return OAuthURL
+  * client will be redirected to Reddit to grant application permission
+  * backend will listen auto redirect on approval to store token
+**/
+
 function LinkRedditForm() {
-  /*
-  TODO:
+    const redditOAuthHandler = async () => null;
 
-    * func will to trigger OAuth request via backend and return OAuthURL
-    * client will be redirected to Reddit to grant application permission
-    * backend will listen auto redirect on approval to store token
-  **/
-  const redditOAuthHandler = async () => null;
-
-  return (
-    <>
-      <h1>Link Reddit Form</h1>
-      <button onClick={redditOAuthHandler}>Link your Reddit Account</button>
-    </>
-  );
+    return (
+        <>
+            <h1>Link Reddit Form</h1>
+            <button onClick={redditOAuthHandler}>Link your Reddit Account</button>
+        </>
+    );
 }
 
 export default LinkRedditForm;
