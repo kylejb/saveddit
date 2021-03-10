f8eb74aa0cdd847058d13005c4dcb053b8eb50cf
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-10 00:48:29 -0500

    BE: Updated endpoint to mirror recent changes

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -6,6 +6,6 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get("/api/v1")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello API Endpoint"}
+    assert response.json() == {"message": "Hellow Root"}
