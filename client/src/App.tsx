f36535a0dac3b8747332cb275b187da007038776
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-11 00:35:27 -0500

    FE: Conditionally render Login or LinkReddit Form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
+import Form from 'components/form/Form';
 import LinkRedditForm from 'LinkRedditForm';
 
 function App(): JSX.Element {
+    const [token, setAuthToken] = useState<string>('');
+
     return (
         <>
             <h1>App Component</h1>
-            <LinkRedditForm />
+            {token ? <LinkRedditForm /> : <Form setAuthToken={setAuthToken} />}
         </>
     );
 }
