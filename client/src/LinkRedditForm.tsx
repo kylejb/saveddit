acf9b91be3ed541b47f1e5fa8486d5f3fd7a83b9
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-20 22:20:32 -0400

    FE: Successfully gets User's savedPostIDs [WIP]

--- client/src/LinkRedditForm.tsx
+++ client/src/LinkRedditForm.tsx
@@ -5,9 +5,36 @@ TODO:
   * client will be redirected to Reddit to grant application permission
   * backend will listen auto redirect on approval to store token
 **/
+import { useState } from 'react';
 
-const LinkRedditForm = (): JSX.Element => {
-    const redditOAuthHandler = async () => null;
+type LinkRedditFormProps = {
+    token: string;
+};
+
+const LinkRedditForm = ({ token }: LinkRedditFormProps): JSX.Element => {
+    const [authLink, setAuthLink] = useState('https://www.reddit.com/api');
+
+    const redditOAuthHandler = async () => {
+        const options = {
+            headers: {
+                Authorization: 'Bearer ' + token,
+            },
+        };
+        const response = await fetch('http://localhost:8000/api/reddit/authenticate', options);
+        const link = await response.json();
+        setAuthLink(link);
+    };
+
+    const clickHandler = async () => {
+        const options = {
+            headers: {
+                Authorization: 'Bearer ' + token,
+            },
+        };
+        const response = await fetch('http://localhost:8000/api/reddit', options);
+        const data = await response.json();
+        console.log('clicked', data);
+    };
 
     return (
         <>
@@ -15,6 +42,10 @@ const LinkRedditForm = (): JSX.Element => {
             <button type='button' onClick={redditOAuthHandler}>
                 Link your Reddit Account
             </button>
+            <a target='_blank' rel='noreferrer' href={authLink}>
+                Hi
+            </a>
+            <button onClick={clickHandler}>Test</button>
         </>
     );
 };
